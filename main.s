4e86c7a79c2b4e6c8d4700e088974fbbd975a5fe
aem1418 <aem1418@skbl-422-007.ph.ic.ac.uk>
2021-11-30 12:05:33 +0000

    Coded interrupt that is called when we change switch. But when we added delay, code stopped working, when we comment out new coed, the debugger shows the code is not going to start when it should.

--- main.s
+++ main.s
@@ -1,15 +1,15 @@
 #include <xc.inc>
 
-extrn	DAC_Setup, DAC_Int_Hi
+extrn	DAC_Setup, DAC_Int_Hi, delay, delay2
 
 psect	code, abs
 rst:	org	0x0000	; reset vector
-	goto	start
+	goto	setup
 
 int_hi:	org	0x0008	; high vector, no low vector
 	goto	DAC_Int_Hi
 	
-start:	call	DAC_Setup
+setup:	call	DAC_Setup
 	goto	$	; Sit in infinite loop
 
 	end	rst
