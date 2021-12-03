3e7279bb790527dae121dd60a1d143c09737196d
aem1418 <aem1418@skbl-422-007.ph.ic.ac.uk>
2021-12-03 12:01:20 +0000

    drafting nurse subroutines

--- main.s
+++ main.s
@@ -13,3 +13,31 @@ setup:	call	DAC_Setup
 	goto	$	; Sit in infinite loop
 
 	end	rst
+
+	
+; creating FallInterrupt subroutine in Nurse - but for the moment, triggered by a button press bc imu not interfaced
+
+;	uart sends message to nurse (stored somewhere)
+;	nurse sees that there's data
+;	trigger interrupt
+;	read (somewhere)
+;	if statetements -> the right subroutine called
+	
+
+; Nurse_fall subroutine - called on the nurse device when the client falls
+	; nurse buzzer and yellow led triggered
+	; nurse lcd lights up
+	
+; Nurse_alert subroutine - called on the nurse device when the client falls
+	; nurse buzzer and yellow led triggered
+	; nurse lcd lights up
+	
+; Nurse_disable subroutine - called on the nurse device when the client falls
+	; nurse buzzer and yellow led triggered
+	; nurse lcd lights up
+; Nurse_
+	
+	
+; buzzer, orange led light up
+	
+; nurse lcd displays message
