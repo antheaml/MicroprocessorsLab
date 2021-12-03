6015151688529fcf817eb43af9daee1bb38de78d
aem1418 <aem1418@skbl-422-007.ph.ic.ac.uk>
2021-12-03 16:51:58 +0000

    Written LED setup connecting the leds on breadboard to PORTD on microprocessor Written 3 new functions: nurse_fall, nurse_alert, nurse_remote_disable Need to wire up nurse_ local_disable with simple button Need to add LCD messages to functions

--- main.s
+++ main.s
@@ -10,9 +10,12 @@ int_hi:	org	0x0008	; high vector, no low vector
 	goto	DAC_Int_Hi
 	
 setup:	call	DAC_Setup
+	call	led_setup
+	call	nurse_alert ; for testing
+	;call	nurse_fall ; for testing
 	goto	$	; Sit in infinite loop
 
-	end	rst
+	
 
 	
 ; creating FallInterrupt subroutine in Nurse - but for the moment, triggered by a button press bc imu not interfaced
@@ -25,19 +28,50 @@ setup:	call	DAC_Setup
 	
 
 ; Nurse_fall subroutine - called on the nurse device when the client falls
-	; nurse buzzer and yellow led triggered
+	; nurse buzzer and red led triggered
 	; nurse lcd lights up
+	
+led_setup:
+    movlw   0b11111100 ; RD0 and RD1 as output
+    movwf   TRISD, A 
+    movlw   0b00000000 ; all portD LEDs off
+    movwf   PORTD, A
+    return
+    ; set all pins to 0
+	
+nurse_fall:
+    ; need rd1 high rest low
+    movlw   0b00000010
+    movwf   PORTD, A
+    return
+    
+nurse_alert:
+    ; need rd0 high rest low
+    movlw   0b11111100 ; RD0 and RD1 as output
+    movwf   TRISD, A 
+    movlw   0b00000001
+    movwf   PORTD, A
+    return
+    
+nurse_remote_disable:
+    ; need all low
+    movlw   0b00000000
+    movwf   PORTD, A
+    return
+    
 	
 ; Nurse_alert subroutine - called on the nurse device when the client falls
 	; nurse buzzer and yellow led triggered
 	; nurse lcd lights up
 	
 ; Nurse_disable subroutine - called on the nurse device when the client falls
-	; nurse buzzer and yellow led triggered
-	; nurse lcd lights up
+	; nurse buzzer and yellow led turned off
+	; nurse lcd turns off
 ; Nurse_
 	
 	
 ; buzzer, orange led light up
 	
 ; nurse lcd displays message
+	
+	    end	rst
