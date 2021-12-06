d66a72c62ca452c052416369e159fb702c727d6b
aem1418 <aem1418@skbl-422-007.ph.ic.ac.uk>
2021-12-06 10:59:31 +0000

    Added LCD messages and subroutines Builds, but does not show the message on LCD - might be an issue with myArray - compare with the one we wrote to fix Still need to write "if statement" for which subroutine to call within the  interrupt

--- lcd_main.s
+++ lcd_main.s
@@ -1,10 +1,12 @@
 #include <xc.inc>
 
-global	setup_lcd
+global	setup_lcd, start_fall_lcd, start_alertButton_lcd, start_disable_lcd
+
 
 extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
 extrn	LCD_Setup, LCD_Write_Message
 extrn   ledSetup, nurse_fall, nurse_alert, nurse_remote_disable
+
 	
 psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
@@ -24,18 +26,18 @@ myTable_fall:
 myTable_disable: 
 	db	'D','e','v','i','c','e',' ','i','d','l','e',0x0a
 					; message, plus carriage return
-	myTable_l   EQU	12	; length of data
+	myTable_2   EQU	12	; length of data
 	align	2
 	
 myTable_alertButton: 
 	db	'H','e','l','p',' ','i','n',' ','R','o','o','m',' ','1','0','8',0x0a
 					; message, plus carriage return
-	myTable_l   EQU	17	; length of data
+	myTable_3   EQU	17	; length of data
 	align	2
     
 psect	code, abs	
 rst: 	org 0x0
- 	goto	setup
+ 	goto	setup_lcd
 
 	; ******* Programme FLASH read Setup Code ***********************
 setup_lcd:	
@@ -57,7 +59,7 @@ start_fall_lcd:
 	movwf	TBLPTRL, A		; load low byte to TBLPTRL
 	movlw	myTable_l	; bytes to read
 	movwf 	counter, A		; our counter register
-	goto	loop
+	goto	loop_fall_lcd
 	
 	
 start_disable_lcd: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
@@ -67,9 +69,9 @@ start_disable_lcd: 	lfsr	0, myArray	; Load FSR0 with address in RAM
 	movwf	TBLPTRH, A		; load high byte to TBLPTRH
 	movlw	low(myTable_disable)	; address of data in PM
 	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
+	movlw	myTable_2	; bytes to read
 	movwf 	counter, A		; our counter register
-	goto	loop
+	goto	loop_disable_lcd
 	
 	
 start_alertButton_lcd: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
@@ -79,20 +81,53 @@ start_alertButton_lcd: 	lfsr	0, myArray	; Load FSR0 with address in RAM
 	movwf	TBLPTRH, A		; load high byte to TBLPTRH
 	movlw	low(myTable_alertButton)	; address of data in PM
 	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
+	movlw	myTable_3	; bytes to read
 	movwf 	counter, A		; our counter register
-	goto	loop
+	goto	loop_alertButton_lcd
 	
-loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+loop_fall_lcd: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
 	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
 	decfsz	counter, A		; count down to zero
-	bra	loop		; keep going until finished
+	bra	loop_fall_lcd		; keep going until finished
 		
 	movlw	myTable_l	; output message to UART
 	lfsr	2, myArray
 	call	UART_Transmit_Message
 
 	movlw	myTable_l	; output message to LCD
+	addlw	0xff		; don't send the final carriage return to LCD
+	lfsr	2, myArray
+	call	LCD_Write_Message
+
+	goto	$		; goto current line in code
+
+loop_disable_lcd: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
+	decfsz	counter, A		; count down to zero
+	bra	loop_disable_lcd		; keep going until finished
+		
+	movlw	myTable_2	; output message to UART
+	lfsr	2, myArray
+	call	UART_Transmit_Message
+
+	movlw	myTable_2	; output message to LCD
+	addlw	0xff		; don't send the final carriage return to LCD
+	lfsr	2, myArray
+	call	LCD_Write_Message
+
+	goto	$		; goto current line in code
+	
+	
+loop_alertButton_lcd: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
+	decfsz	counter, A		; count down to zero
+	bra	loop_alertButton_lcd		; keep going until finished
+		
+	movlw	myTable_3	; output message to UART
+	lfsr	2, myArray
+	call	UART_Transmit_Message
+
+	movlw	myTable_3	; output message to LCD
 	addlw	0xff		; don't send the final carriage return to LCD
 	lfsr	2, myArray
 	call	LCD_Write_Message
