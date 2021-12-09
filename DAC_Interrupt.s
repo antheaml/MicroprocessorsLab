5f68bb8d290b792e254d909663053eb2353d69e7
aem1418 <aem1418@skbl-422-007.ph.ic.ac.uk>
2021-12-09 11:14:23 +0000

    In each interrupt subroutine: 	- Disabled interrupt before LCD message is sent 	- Enabled interrupt after LCD message is sent again Added check functions to check which subroutine should be triggered, based on the signal received via uart from the client device UART will arrive at PORTG. we save this binary number to signal_code data space Least significant bit is always 1, RG0 is connected to interrupt pin (RB0) via cable so when a message arrives, we trigger interrupt subroutine.

--- DAC_Interrupt.s
+++ DAC_Interrupt.s
@@ -7,24 +7,38 @@ extrn	nurse_fall, nurse_alert, nurse_remote_disable
 psect udata_acs
 delay_counter1:	    ds 1
 delay_counter2:	    ds 1
+alert_code:	    ds 1
 
 psect	dac_code, class=CODE
 	
 DAC_Int_Hi:	
 	btfss	RBIF		; check that this is RB interrupt
 	retfie	f		; if not then return
-	
-	call	nurse_fall
-	
-;	movlw	0xff
-;	movwf	PORTE, A
-; 	movlw	0xff
-;	movwf	delay_counter1, A
-;	call	delay
-;	movlw	0x00
-;	movwf	PORTE, A
+	; logic to figure out which subroutine to call depending on signal
+	movff	PORTG, alert_code
+	call	check_fall
+	call	check_alert_button
+	call	check_disable_button
 	bcf	RBIF		; clear interrupt flag
 	retfie	f		; fast return from interrupt
+	
+check_fall:
+    movlw   0b00000001
+    CPFSEQ alert_code, A ; alert code is the code sent from the client to nurse, if alert_code = 1, will call nurse_fall, else will return
+    return
+    call    nurse_fall
+    
+check_alert_button:
+    movlw   0b00000011
+    CPFSEQ alert_code, A ; alert code is the code sent from the client to nurse, if alert_code = 2, will call nurse_fall, else will return
+    return
+    call    nurse_alert
+    
+check_disable_button:
+    movlw   0b00000111
+    CPFSEQ  alert_code, A ; alert code is the code sent from the client to nurse, if alert_code = 2, will call nurse_fall, else will return
+    return
+    call    nurse_remote_disable
 
 delay:
     movlw	0xff
