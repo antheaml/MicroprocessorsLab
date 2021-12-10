110a7bea1ddb98f6995701e2486e2bacb2ebb5a5
aem1418 <aem1418@skbl-422-007.ph.ic.ac.uk>
2021-12-10 11:53:48 +0000

    SPI code from the powerpoint presentation put in

--- main.s
+++ main.s
@@ -1,22 +1,33 @@
 #include <xc.inc>
 
-extrn	spi_setup
+extrn	SPI_MasterInit
 
 psect	code, abs
 	
 rst:	org	0x0000	; reset vector
-	goto	globalSetup
+	goto	sending_data
 
+main:
+    call    SPI_MasterInit
+    call    sending_data
+    
+    
+sending_data:
+    ; INT_EN register
+    ;pull csb low - start
+    movlw   0x50
+    call    SPI_MasterTransmit
+    movlw   0b00000000
+    call    SPI_MasterTransmit
+    ;pull csb high - stop
+    movlw   0b00001111
+    call    SPI_MasterTransmit
+    
+    
+    call    SPI_MasterInit
+    call    SPI_MasterTransmit
+    
+	
 
-;int_hi:	org	0x0008	; high vector, no low vector,
-;	goto	DAC_Int_Hi
-;	
-globalSetup:
-;	movlw	0b11111111
-;	movwf	TRISB
-;	movlw	0b00000000
-;	movwf	PORTB
-	call	spi_setup
-	goto	$	; Sit in infinite loop
 
 end	rst
