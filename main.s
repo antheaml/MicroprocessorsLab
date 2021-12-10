1c43de71a7f39bedb729ce4f19b7d31443b68874
aem1418 <aem1418@skbl-422-007.ph.ic.ac.uk>
2021-12-10 15:42:57 +0000

    Adapted SPI code to write IMU setup orders - IMU still doesn't raise any interrupts though. added SPI_MasterRead function - will need to test using UART which we will add next

--- main.s
+++ main.s
@@ -1,31 +1,84 @@
 #include <xc.inc>
 
-extrn	SPI_MasterInit
+extrn	SPI_MasterInit, SPI_MasterTransmit
 
 psect	code, abs
 	
 rst:	org	0x0000	; reset vector
-	goto	sending_data
+	goto	main
 
 main:
+    bsf	    TRISB, 0, A
     call    SPI_MasterInit
     call    sending_data
+    goto    $
     
     
 sending_data:
-    ; INT_EN register
-    ;pull csb low - start
-    movlw   0x50
+    call    INT_EN
+    call    INT_OUT_CTRL
+    call    INT_MAP
+    ;call   INT_LOW_HIGH
+    return
+
+INT_EN:
+    ;################# INT_EN[0]
+    bcf	    PORTE, 0, A		; pull CSB low - start message
+    movlw   0x50 ; IF WE EVER WANT TO READ, CHANGE MSB TO 1 
     call    SPI_MasterTransmit
     movlw   0b00000000
     call    SPI_MasterTransmit
-    ;pull csb high - stop
+    bsf	    PORTE, 0, A		; pull CSB high - stop message
+    ;################# INT_EN[1]
+    bcf	    PORTE, 0, A		; pull CSB low - start message
+    movlw   0x51
+    call    SPI_MasterTransmit
     movlw   0b00001111
     call    SPI_MasterTransmit
-    
-    
-    call    SPI_MasterInit
+    bsf	    PORTE, 0, A		; pull CSB high - stop message
+    ;################# INT_EN[2]
+    bcf	    PORTE, 0, A		; pull CSB low - start message
+    movlw   0x52
     call    SPI_MasterTransmit
+    movlw   0b00000000
+    call    SPI_MasterTransmit
+    bsf	    PORTE, 0, A		; pull CSB high - stop message
+    return
+
+INT_OUT_CTRL:
+    ;################# INT_OUT_CTRL
+    bcf	    PORTE, 0, A		; pull CSB low - start message
+    movlw   0x53
+    call    SPI_MasterTransmit
+    movlw   0b00001010
+    call    SPI_MasterTransmit
+    bsf	    PORTE, 0, A		; pull CSB high - stop message
+    return
+    
+    
+INT_MAP:
+    ;################# INT_MAP[0]
+    bcf	    PORTE, 0, A		; pull CSB low - start message
+    movlw   0x55 ; IF WE EVER WANT TO READ, CHANGE MSB TO 1 
+    call    SPI_MasterTransmit
+    movlw   0b11111111
+    call    SPI_MasterTransmit
+    bsf	    PORTE, 0, A		; pull CSB high - stop message
+    ;################# INT_MAP[1]
+    bcf	    PORTE, 0, A		; pull CSB low - start message
+    movlw   0x56
+    call    SPI_MasterTransmit
+    movlw   0b11110000
+    call    SPI_MasterTransmit
+    bsf	    PORTE, 0, A		; pull CSB high - stop message
+    ;################# INT_MAP[2]
+    bcf	    PORTE, 0, A		; pull CSB low - start message
+    movlw   0x57
+    call    SPI_MasterTransmit
+    movlw   0b00000000
+    call    SPI_MasterTransmit
+    bsf	    PORTE, 0, A		; pull CSB high - stop message
+    return
     
 	
 
--- spi.s
+++ spi.s
@@ -1,7 +1,11 @@
 #include <xc.inc>
     
-global	SPI_MasterInit
+global	SPI_MasterInit, SPI_MasterTransmit
 
+psect	spi_data
+    ds	read_byte1: 1
+psect	spi_code, class=CODE
+    
 SPI_MasterInit:	; Set Clock edge to negative
 	bcf	CKE2	; CKE bit in SSP2STAT, 
 	; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
@@ -10,14 +14,25 @@ SPI_MasterInit:	; Set Clock edge to negative
 	; SDO2 output; SCK2 output
 	bcf	TRISD, PORTD_SDO2_POSN, A	; SDO2 output #### can just wire up port e pins to port c 
 	bcf	TRISD, PORTD_SCK2_POSN, A	; SCK2 output
+	;######### added by us
+	bcf	TRISE, 0, A
+	bsf	PORTE, 0, A
+	;######### added by us
 	return
 
 SPI_MasterTransmit:  ; Start transmission of data (held in W)
 	movwf 	SSP2BUF, A 	; write data to output buffer
-	
-	;
 Wait_Transmit:	; Wait for transmission to complete 
 	btfss 	SSP2IF		; check interrupt flag to see if data has been sent
 	bra 	Wait_Transmit
+	bcf 	SSP2IF		; clear interrupt flag
+	return
+
+SPI_MasterRead:
+	movwf	SSP2BUF, A
+Wait_Read:	; Wait for transmission + read to complete 
+	btfss 	SSP2IF		; check interrupt flag to see if data has been sent
+	bra 	Wait_Read
+	movff	SSP2BUF, read_byte1, A	    ; load SSP2BUF to read_byte1
 	bcf 	SSP2IF		; clear interrupt flag
 	return
