a8d858f9276f5e3efc0aa3fba083d458f20bc8ce
aem1418 <aem1418@skbl-422-007.ph.ic.ac.uk>
2021-12-13 17:58:40 +0000

    More testing to get SPI to work

--- spi.s
+++ spi.s
@@ -15,6 +15,7 @@ SPI_MasterInit:	; Set Clock edge to negative
 	; SDO2 output; SCK2 output
 	bcf	TRISC, PORTC_SDO1_POSN, A	; SDO2 output #### can just wire up port e pins to port c 
 	bcf	TRISC, PORTC_SCK1_POSN, A	; SCK2 output
+	;bsf	TRISC, PORTC_SDI1_POSN, A
 	;######### added by us
 	bcf	TRISE, 0, A
 	bsf	PORTE, 0, A
@@ -30,12 +31,12 @@ Wait_Transmit:	; Wait for transmission to complete
 	return
 
 SPI_MasterRead:
-	movwf	SSP1BUF, A
+	movwf	SSP1BUF, A ; send register we would like to access
 Wait_Read:	; Wait for transmission + read to complete 
 	btfss 	SSP1IF	; check interrupt flag to see if data has been sent
 	bra 	Wait_Read
-	movff	SSP1BUF, read_byte1, A	    ; load SSP2BUF to read_byte1
+	;movff	SSP1BUF, read_byte1, A	    ; load SSP2BUF to read_byte1
+Send_to_Master:
 	movff	SSP1BUF, WREG, A
 	bcf 	SSP1IF		; clear interrupt flag
-	;send read_byte1 to uart
 	return
