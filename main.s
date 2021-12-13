12eb98d42508ec2f8d304e74f26afbae22edc34e
aem1418 <aem1418@skbl-422-007.ph.ic.ac.uk>
2021-12-13 17:58:47 +0000

    More testing to get SPI to work

--- main.s
+++ main.s
@@ -21,11 +21,19 @@ main:
    
 test_read:
     ;################# CHIP_ID address 0x00 but MSB = 1 (Read)
+    call    NOP_delay
+    call    NOP_delay
     bcf	    PORTE, 0, A		; pull CSB low - start message
-    movlw   0b10000000	; chip id
+    call    NOP_delay
+    call    NOP_delay
+    ;bsf	    TRISC, 4, A ; set RC4 as input, since this will be recieving data from the slave ???????????
+    movlw   0b10000000	; chip id register
     ;movlw   0b10101010
-;    movlw   0b10000011	; pmu status
+    ;movlw   0b10000011	; pmu status
+    call    SPI_MasterTransmit
+    movlw   0b00000000 ; so we don't write anything while trying to read
     call    SPI_MasterRead
+    bsf	    PORTE, 0, A		; pull CSB high - end message
     ;movff   read_byte1, WREG   
     call    UART_Transmit_Byte
     bsf	    PORTE, 0, A		; pull CSB high - stop message
@@ -45,55 +53,11 @@ INT_EN:
     call    SPI_MasterTransmit
     movlw   0b00000000
     call    SPI_MasterTransmit
-    bsf	    PORTE, 0, A
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    bsf	    PORTE, 0, A    
+    call    NOP_delay
+    call    NOP_delay
+    call    NOP_delay
+    call    NOP_delay
     ; pull CSB high - stop message
     ;################# INT_EN[1]
     bcf	    PORTE, 0, A		; pull CSB low - start message
@@ -102,14 +66,10 @@ INT_EN:
     movlw   0b00001111
     call    SPI_MasterTransmit
     bsf	    PORTE, 0, A		; pull CSB high - stop message
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    call    NOP_delay
+    call    NOP_delay
+    call    NOP_delay
+    call    NOP_delay
     ;################# INT_EN[2]
     bcf	    PORTE, 0, A		; pull CSB low - start message
     movlw   0x52
@@ -117,14 +77,10 @@ INT_EN:
     movlw   0b00000000
     call    SPI_MasterTransmit
     bsf	    PORTE, 0, A		; pull CSB high - stop message
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    call    NOP_delay
+    call    NOP_delay
+    call    NOP_delay
+    call    NOP_delay
     return
 
 INT_OUT_CTRL:
@@ -135,14 +91,10 @@ INT_OUT_CTRL:
     movlw   0b00001010
     call    SPI_MasterTransmit
     bsf	    PORTE, 0, A		; pull CSB high - stop message
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    call    NOP_delay
+    call    NOP_delay
+    call    NOP_delay
+    call    NOP_delay
     return
     
     
@@ -154,14 +106,10 @@ INT_MAP:
     movlw   0b11111111
     call    SPI_MasterTransmit
     bsf	    PORTE, 0, A		; pull CSB high - stop message
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    call    NOP_delay
+    call    NOP_delay
+    call    NOP_delay
+    call    NOP_delay
     ;################# INT_MAP[1]
     bcf	    PORTE, 0, A		; pull CSB low - start message
     movlw   0x56
@@ -169,14 +117,10 @@ INT_MAP:
     movlw   0b11110000
     call    SPI_MasterTransmit
     bsf	    PORTE, 0, A		; pull CSB high - stop message
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    call    NOP_delay
+    call    NOP_delay
+    call    NOP_delay
+    call    NOP_delay
     ;################# INT_MAP[2]
     bcf	    PORTE, 0, A		; pull CSB low - start message
     movlw   0x57
@@ -184,17 +128,35 @@ INT_MAP:
     movlw   0b00000000
     call    SPI_MasterTransmit
     bsf	    PORTE, 0, A		; pull CSB high - stop message
+    call    NOP_delay
+    call    NOP_delay
+    call    NOP_delay
+    call    NOP_delay
+    return
+    
+NOP_delay:
+    ;4 clock cycles
+    ;1
     nop
     nop
     nop
     nop
+    ;2
+    nop
+    nop
+    nop
+    nop
+    ;3
+    nop
+    nop
+    nop
+    nop
+    ;4
     nop
     nop
     nop
     nop
     return
-    
-	
 
 
 end	rst
