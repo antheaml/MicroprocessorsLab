74537e4aa46269529c36a47006bbb29ac8727195
aem1418 <aem1418@skbl-422-007.ph.ic.ac.uk>
2021-12-13 11:30:36 +0000

    Trying to test SPI. Connected to SSP 2 not 1

--- spi.s
+++ spi.s
@@ -1,15 +1,15 @@
 #include <xc.inc>
     
-global	SPI_MasterInit, SPI_MasterTransmit, SPI_MasterRead;, read_byte1
+global	SPI_MasterInit, SPI_MasterTransmit, SPI_MasterRead, read_byte1
 
-;psect	spi_data
-;read_byte1: ds	1
+psect	spi_data
+    read_byte1: ds	1
     
 psect	spi_code, class=CODE
     
 SPI_MasterInit:	; Set Clock edge to negative
 	bcf	CKE2	; CKE bit in SSP2STAT, 
-	; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+	;MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
 	movlw 	(SSP2CON1_SSPEN_MASK)|(SSP2CON1_CKP_MASK)|(SSP2CON1_SSPM1_MASK)
 	movwf 	SSP2CON1, A
 	; SDO2 output; SCK2 output
@@ -32,9 +32,9 @@ Wait_Transmit:	; Wait for transmission to complete
 SPI_MasterRead:
 	movwf	SSP2BUF, A
 Wait_Read:	; Wait for transmission + read to complete 
-	btfss 	SSP2IF		; check interrupt flag to see if data has been sent
+	btfss 	SSP2IF	; check interrupt flag to see if data has been sent
 	bra 	Wait_Read
-	;movff	SSP2BUF, read_byte1, A	    ; load SSP2BUF to read_byte1
+	movff	SSP2BUF, read_byte1, A	    ; load SSP2BUF to read_byte1
 	movff	SSP2BUF, WREG, A
 	bcf 	SSP2IF		; clear interrupt flag
 	;send read_byte1 to uart
